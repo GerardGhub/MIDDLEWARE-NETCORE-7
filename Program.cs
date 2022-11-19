d9b37872ff9e434be31cb6c82ffef1b931c25498
GerardGhub <singian28@gmail.com>
2022-11-19 17:04:55 +0800

    Middleware Chain

--- Program.cs
+++ Program.cs
@@ -1,6 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//Middleware 1
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+  await context.Response.WriteAsync("Hello");
+    await next(context);
+});
+
+//Middleware 2
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("Hello 2 times");
+    await next(context);
+});
+
+//http redirection/ authorization
+//Middleware 3
+app.Run(async (HttpContext context) =>
+{
+    await context.Response.WriteAsync("Hello Again");
+
+});
 
 app.Run();
